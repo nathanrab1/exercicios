39e23acfb745f1290ab44a871218d2174167cde0
nathanrab1 <nathanrab@gmail.com>
2023-10-14 13:36:45 -0300

    123

--- js/java.js
+++ js/java.js
@@ -1,34 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').addEventListener('submit', function(event) {
-        let name = document.querySelector('#name').value;
-        alert('hello, ' + name);
-        event.preventDefault();
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('form').addEventListener('submit', function(event) {
+//         let name = document.querySelector('#name').value;
+//         alert('hello, ' + name);
+//         event.preventDefault();
+//     });
+// });
 
-document.querySelector('#red').addEventListener('click', function() {
-    document.getElementById("circulo").style.backgroundColor = "red";
-});
-document.querySelector('#green').addEventListener('click', function() {
-    document.getElementById("circulo").style.backgroundColor = "green";
-});
-document.querySelector('#blue').addEventListener('click', function() {
-    document.getElementById("circulo").style.backgroundColor = "blue";
-});
+// document.querySelector('#red').addEventListener('click', function() {
+//     document.getElementById("circulo").style.backgroundColor = "red";
+// });
+// document.querySelector('#green').addEventListener('click', function() {
+//     document.getElementById("circulo").style.backgroundColor = "green";
+// });
+// document.querySelector('#blue').addEventListener('click', function() {
+//     document.getElementById("circulo").style.backgroundColor = "blue";
+// });
 
 
 const container = document.getElementById('container');
 const createCirclesBtn = document.getElementById('createCirclesBtn');
 
-createCirclesBtn.addEventListener('click', function () {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 14; j++) {
-      const circle = document.createElement('div');
-      circle.classList.add('circle');
-      circle.addEventListener('mouseover', function() {
-        circle.classList.add('hovered');
-      });
-      container.appendChild(circle);
-    }
-  }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 14; j++) {
+          const circle = document.createElement('div');
+          circle.classList.add('circle');
+          circle.addEventListener('mouseover', function() {
+            circle.classList.add('hovered');
+          });
+          container.appendChild(circle);
+        }
+      }    
+
+
 });
+
+// createCirclesBtn.addEventListener('click', function () {
+//     for (let i = 0; i < 4; i++) {
+//       for (let j = 0; j < 14; j++) {
+//         const circle = document.createElement('div');
+//         circle.classList.add('circle');
+//         circle.addEventListener('mouseover', function() {
+//           circle.classList.add('hovered');
+//         });
+//         container.appendChild(circle);
+//       }
+//     }
+//   });
