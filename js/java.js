84eb0eae62f2428419713681c92793b43a878998
nathanrab1 <nathanrab@gmail.com>
2023-10-14 13:59:06 -0300

    12345

--- js/java.js
+++ js/java.js
@@ -1,30 +1,8 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.querySelector('form').addEventListener('submit', function(event) {
-//         let name = document.querySelector('#name').value;
-//         alert('hello, ' + name);
-//         event.preventDefault();
-//     });
-// });
-
-// document.querySelector('#red').addEventListener('click', function() {
-//     document.getElementById("circulo").style.backgroundColor = "red";
-// });
-// document.querySelector('#green').addEventListener('click', function() {
-//     document.getElementById("circulo").style.backgroundColor = "green";
-// });
-// document.querySelector('#blue').addEventListener('click', function() {
-//     document.getElementById("circulo").style.backgroundColor = "blue";
-// });
-
-
 const container = document.getElementById('container');
 const createCirclesBtn = document.getElementById('createCirclesBtn');
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 14; j++) {
+    for (let i = 0; i < 40; i++) {
+        for (let j = 0; j < 30; j++) {
           const circle = document.createElement('div');
           circle.classList.add('circle');
           circle.addEventListener('mouseover', function() {
@@ -33,19 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
           container.appendChild(circle);
         }
       }    
-
-
 });
-
-// createCirclesBtn.addEventListener('click', function () {
-//     for (let i = 0; i < 4; i++) {
-//       for (let j = 0; j < 14; j++) {
-//         const circle = document.createElement('div');
-//         circle.classList.add('circle');
-//         circle.addEventListener('mouseover', function() {
-//           circle.classList.add('hovered');
-//         });
-//         container.appendChild(circle);
-//       }
-//     }
-//   });
